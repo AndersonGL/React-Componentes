ee0c80806996ac6585f5fa45c0d0e6e3a854611a
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2023-10-26 00:10:51 -0300

    commit

---
+++ src/componentes/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+
+export default function Footer() {
+  return (
+
+    <footer> Desenvolvido com react </footer>
+
+
+ 
+  );
+
+  
+}
+
---
+++ src/componentes/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css'
+
+function Header() {
+  return (
+    
+<header>Trabalhando com componentes</header>
+
+ 
+  );
+
+  
+}
+
+export default Header;
+
